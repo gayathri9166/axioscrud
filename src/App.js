e0cfb4c8826be547f7d44b2d1501769b8872e3b7
Gayathri Gujjari <gayathri.gujjari@otsi.com>
2023-11-15 15:57:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [name, setName] = useState();
+  const [api, setApi] = useState([]);
+
+  const postName = () => {
+    axios.post("https://620654a192dd6600171c09b4.mockapi.io/username", {
+      name
+    });
+    // console.log(name)
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://620654a192dd6600171c09b4.mockapi.io/username")
+      .then((res) => setApi(res.data));
+  }, []);
+
+  const delName = (id) => {
+    axios
+      .delete(`https://620654a192dd6600171c09b4.mockapi.io/${id}`)
+      .then(() => getData())
+      .catch((err) => console.log(err));
+  };
+  const getData = () => {
+    axios
+      .get("https://620654a192dd6600171c09b4.mockapi.io/usernam")
+      .then((getdata) => setApi(getdata.data));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Type..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={postName}>Submit</button>
+      {api.map((val) => {
+        return (
+          <>
+            <p>{val.name}</p>
+            <button onClick={() => delName(val.id)}>Del</button>
+          </>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
